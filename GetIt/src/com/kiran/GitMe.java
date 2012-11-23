3a6ef40c111adf2bee5e36e5ca33bd8e8ccc01d4
kikanapa <kikanapa@kikanapa-WS.cisco.com>
2012-11-23 02:22:37 -0800

    Initial Commit.

---
+++ GetIt/src/com/kiran/GitMe.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.kiran;
+
+/**
+ * @author kikanapa
+ *
+ */
+public class GitMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is GitMe Plug-in");
+	}
+
+}
